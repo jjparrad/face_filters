32ebc8aec791fd4be8b487e02897d6da0d205606
Andrew Mansour <andrew.mansour24@gmail.com>
2025-11-11 10:54:36 +0100

    first script + project structure

---
+++ src/age_detection.py
@@ -0,0 +1,11 @@
+"""
+This is a first test script that will be guided by this tutorial video : https://www.youtube.com/watch?v=ivFuOQcBiN8
+
+The objective is to detect gender and approximate age on a given face. This may or may not be useful for the rest need to see. 
+This is mostly just to familiarize ourselves with image based AI and commonly used libraries.
+
+Dependencies : - OpenCV 
+
+"""
+
+print("Hello World !")
